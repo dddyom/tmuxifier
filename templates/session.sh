db531d346b5a765a981ef324056a8957c5701a1c
dddyom <dddyom@gmail.com>
2023-05-05 17:18:04 +0600

    change templates + add runner using fzf

--- templates/session.sh
+++ templates/session.sh
@@ -1,21 +1,10 @@
-# Set a custom session root path. Default is `$HOME`.
-# Must be called before `initialize_session`.
-#session_root "~/Projects/{{SESSION_NAME}}"
+session_root "{{SESSION_ROOT}}"
 
-# Create session with specified name if it does not already exist. If no
-# argument is given, session name will be based on layout file name.
 if initialize_session "{{SESSION_NAME}}"; then
 
-  # Create a new window inline within session layout definition.
-  #new_window "misc"
-
-  # Load a defined window layout.
-  #load_window "example"
-
-  # Select the default active window on session creation.
-  #select_window 1
+  # load_window "vi"
+  # load_window "utils"
+  # select_window 1
 
 fi
-
-# Finalize session creation and switch/attach to it.
 finalize_and_go_to_session
--- templates/window.sh
+++ templates/window.sh
@@ -1,12 +1,5 @@
-# Set window root path. Default is `$session_root`.
-# Must be called before `new_window`.
-#window_root "~/Projects/{{WINDOW_NAME}}"
-
-# Create new window. If no argument is given, window name will be based on
-# layout file name.
 new_window "{{WINDOW_NAME}}"
 
-# Split window into panes.
 #split_v 20
 #split_h 50
 
